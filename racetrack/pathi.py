57cc3bbd91fcdd55cfaf5262867f9d1c283bac7a
Xenii <55805676+xen1i@users.noreply.github.com>
2025-01-02 17:14:57 +0100

    added the racetracktiles and some of the functional code

---
+++ racetrack/pathi.py
@@ -0,0 +1,66 @@
+#Copyright 2025 - Caspar Moritz Klein
+import random
+
+# Makes a racetrack map and returns the coordinates of the path the track takes
+# width - width of the map
+# height - height of the map
+# debug - enable debug prints
+# randomize_start - moves the start to a random location on the path; if false, start will be near (0,0)
+def make_loop(width,height,debug=False,randomize_start=True):
+    if debug: print("Generating Path...")
+    free_cell={}
+    for x in range(1,width-1):
+        for y in range(1, height-1):
+            free_cell[(x,y)]=1
+    path=[]
+    for x in range(0,width):
+        path.append((x,0))
+    for y in range(1,height):
+        path.append((width-1,y))
+    for x in range(width-2,-1,-1):
+        path.append((x,height-1))
+    for y in range(height-2,-1,-1):
+        path.append((0,y))
+
+    cracks=random.randint(min(width,height),width+height)
+    if debug: print("Adding Variety...")
+    for i in range(cracks):
+        crack_path(path,free_cell)
+
+    if randomize_start:
+        if debug: print("Randomizing starting position")
+        path=randomize_path_start(path)
+    if debug: print("Finished generating path")
+    if debug: print(path)
+    return path[:-1]
+
+def crack_path(path,free_cells):
+    pos=random.randrange(0,len(path))
+    corn=-1
+    i=pos
+    while True:
+        a = path[i-1]
+        b = path[ i ]
+        c = path[(i+1)%len(path)]
+        if is_corner(a,b,c):
+            if corn >= 0:
+                i=i-1
+                break
+            else:
+                corn=i
+        i=(i+1)%len(path)
+        
+
+def randomize_path_start(path):
+    path = path[:-1]
+    steps=random.randrange(0,len(path))
+    path=path[steps:]+path[:steps]
+    path.append(path[0])
+    return path
+
+def is_corner(a,b,c):
+    return abs(a[0]-b[0])+abs(b[0]-c[0])==1
+
+
+if __name__ == "__main__":
+    make_loop(8,5,True,True)
---
+++ racetrack/racetrack_generator.py
@@ -0,0 +1,79 @@
+from PIL import Image
+import numpy as np
+import random
+import pathi as map_gen
+
+
+def generate_random_map_image(width,height):
+    straight = Image.open("racetrack/Straight.png").convert('L')
+    grass = Image.open("racetrack/Grass.png")
+    curve = Image.open("racetrack/Curve.png")
+    canvas = Image.new('L', (width*100,height*100),255)
+    #print(canvas)
+
+    for i in range(width):
+        for j in range(height):
+            im = random.choice([grass, curve, straight])
+            angle = random.choice([0,90,180,270])
+            flip = random.choice([1, 0])
+            im = im.rotate(angle)
+            if flip == 1:
+                im = im.transpose(Image.FLIP_LEFT_RIGHT)
+            
+            canvas.paste(im,(i*100,j*100))
+    return canvas
+
+def generate_map_image(width,height,path):
+    straight_h = Image.open("racetrack/Straight.png").convert('L')
+    grass = Image.open("racetrack/Grass.png")
+    canvas = Image.new('L', (width*100,height*100),255)
+    #print(canvas)
+
+    straight_v = straight_h.copy()
+    straight_v=straight_v.rotate(90)
+
+    curve1 = Image.open("racetrack/Curve.png") # Left down
+    curve2=curve1.copy()
+    curve2=curve2.rotate(90) # Down Right
+    curve3=curve2.copy()
+    curve3=curve3.rotate(90) # Right Top
+    curve4=curve3.copy()
+    curve4=curve4.rotate(90) # Top Left
+
+    tileDict={
+        (3,0) : straight_h,
+        (-3,0) : straight_h,
+        (0,3) : straight_v,
+        (0,-3) : straight_v,
+        (2,1) : curve1,
+        (-1,-2) : curve1,
+        (-2,1) : curve2,
+        (1,-2) : curve2,
+        (-2,-1) : curve3,
+        (1,2) : curve3,
+        (2,-1) : curve4,
+        (-1,2) : curve4
+    }
+
+    for i in range(width):
+        for j in range(height):
+            canvas.paste(grass,(i*100,j*100))
+
+    for i in range(len(path)):
+        a=path[i-1]
+        b=path[i]
+        c=path[(i+1)%len(path)]
+        cd=((b[0]+c[0]-2*a[0]),(b[1]+c[1]-2*a[1])) # coordinate difference
+        canvas.paste(tileDict.get(cd,grass),(b[0]*100,b[1]*100))
+
+    return canvas
+
+def do_stuff():
+    w=10
+    h=8
+    path=map_gen.make_loop(w,h)
+    canvas=generate_map_image(w,h,path)
+    canvas.save("racetrack/canvas.png")
+
+if __name__=="__main__":
+    do_stuff()
